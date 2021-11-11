70c8182b293fb5eddc423d5d9323c3390e541799
Preyash Patel <preyash@ecosense-enviro.com>
2021-11-11 21:02:37 +0530

    working

--- create_bulk_profile/bulk_profile.py
+++ create_bulk_profile/bulk_profile.py
@@ -8,42 +8,43 @@ obj = GoLogin(options={
     "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MThiYTdmN2RiYmM5MzNlODNkYmM5ZjkiLCJ0eXBlIjoiZGV2Iiwiand0aWQiOiI2MThiYTgxNWRiYmM5MzU1MzlkYmNhMDcifQ.pQueIhjVztexcwzNCSHxu-4X1tvcf5eqFtHFBzYn7X0",
     "profile_id": "618ba7f8dbbc936b21dbc9fb"
 })
-obj.start()
+# obj.start()
 # print(obj.create())
 
-exit()
+# exit()
 orignalDataset = pd.read_csv("profile_dataset_2.csv")
-dataset = orignalDataset[orignalDataset['name'].notnull()][orignalDataset['isCreated'] != True]
+dataset = orignalDataset[orignalDataset['name'].notnull(
+)][orignalDataset['isCreated'] != True]
 
 for i in range(len(dataset.name)):
     host, port = dataset.proxyList.iloc[i].split(":")
     options = {
-        "name": dataset.name.iloc[i],
-        # "notes": "",
-        # "debugMode": False,
-        # "browserType": "chrome",
-        # "os": random.choice(["win", "android", "mac", "lin"]),
-        # "startUrl": random.choice(["google.com", "coindesk.com", "markets.businessinsider.com", "coinbase.com", "economictimes.indiatimes.com"]),
-        # "googleServicesEnabled": random.choice([True, False]),
-        # "lockEnabled": random.choice([True, False]),
-        # "audioContext": {
-        #     "mode": "noise"
-        # },
-        # "canvas": {
-        #     "mode": "noise"
-        # },
-        # "webRTC": {
-        #     "mode": "disabled",
-        #     "enabled": random.choice([True, False]),
-        #     "customize": random.choice([True, False]),
-        #     "fillBasedOnIp": random.choice([True, False])
-        # },
-        # "proxyEnabled": True,
-        # "proxy": {
-        #     "mode": "http",
-        #     "host": host,
-        #     "port": port,
-        # }
+        "name": dataset.name.iloc[i] + str(i*100),
+        "notes": "",
+        "debugMode": False,
+        "browserType": "chrome",
+        "os": random.choice(["win", "android", "mac", "lin"]),
+        "startUrl": random.choice(["google.com", "coindesk.com", "markets.businessinsider.com", "coinbase.com", "economictimes.indiatimes.com"]),
+        "googleServicesEnabled": random.choice([True, False]),
+        "lockEnabled": random.choice([True, False]),
+        "audioContext": {
+            "mode": "noise"
+        },
+        "canvas": {
+            "mode": "noise"
+        },
+        "webRTC": {
+            "mode": "disabled",
+            "enabled": random.choice([True, False]),
+            "customize": random.choice([True, False]),
+            "fillBasedOnIp": random.choice([True, False])
+        },
+        "proxyEnabled": True,
+        "proxy": {
+            "mode": "http",
+            "host": host,
+            "port": port,
+        }
     }
     resultID = obj.create(options)
     time.sleep(2)
@@ -51,6 +52,7 @@ for i in range(len(dataset.name)):
     if(resultID != None):
         print("No Profiles created successfully:", i+1)
         index = orignalDataset.index[orignalDataset['proxyList'] == dataset.proxyList.iloc[i]]
-        orignalDataset.isCreated.iloc[index] = True
+        # orignalDataset.isCreated.iloc[index] = True
 
 orignalDataset.to_csv("profile_dataset_2.csv", index=False)
+print("Profile Creation Done")
