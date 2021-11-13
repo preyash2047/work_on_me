4481d06892210697927f827904c7d41f806a5d5f
Preyash Patel <preyash@ecosense-enviro.com>
2021-11-13 12:38:22 +0530

    done

--- bot_v0.0.3/bot_v0.0.3.py
+++ bot_v0.0.3/bot_v0.0.3.py
@@ -4,6 +4,7 @@ from gologin import GoLogin
 from codeme import CodeMe
 from sys import platform
 import pandas as pd
+from get_key import get_key
 import random
 import time
 import requests
@@ -11,8 +12,8 @@ import json
 
 global gl
 gl = GoLogin(options={
-    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MThiYTdmN2RiYmM5MzNlODNkYmM5ZjkiLCJ0eXBlIjoiZGV2Iiwiand0aWQiOiI2MThiYTgxNWRiYmM5MzU1MzlkYmNhMDcifQ.pQueIhjVztexcwzNCSHxu-4X1tvcf5eqFtHFBzYn7X0",
-    "profile_id": "896d4dd755db3fbacfa87f60"
+    "token": get_key(),
+    "profile_id": ""
 })
 
 if platform == "linux" or platform == "linux2":
